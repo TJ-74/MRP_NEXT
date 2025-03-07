d013b6f29acb9cd0237de84546a4d4a457678376
74run <tarunjanapati7@gmail.com>
2025-03-06 21:38:10 -0600

    push code

---
+++ .next/static/chunks/src_app_searchform_page_tsx_98822a10._.js
@@ -0,0 +1,16 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/src_app_searchform_page_tsx_98822a10._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/src_app_searchform_page_tsx_98822a10._.js",
+  "chunks": [
+    "static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_e59df5be._.js",
+    "static/chunks/node_modules_@firebase_auth_dist_esm2017_698e3292._.js",
+    "static/chunks/node_modules_@firebase_firestore_dist_index_esm2017_c2fcaa2e.js",
+    "static/chunks/node_modules_@firebase_storage_dist_index_esm2017_b3a08d2a.js",
+    "static/chunks/node_modules_09f9542a._.js",
+    "static/chunks/src_1c748133._.js"
+  ],
+  "source": "dynamic"
+});
